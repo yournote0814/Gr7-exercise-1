d8444e93bff7d88dbf7e462997d23b23b3945477
Ryan Saliva <144507791+yournote0814@users.noreply.github.com>
2024-10-05 15:58:36 +0800

    grp7_saliva_ex2.py

---
+++ grp7_saliva_ex2.py
@@ -0,0 +1,27 @@
+"""
+Name:
+BSCS 3F
+
+Exercise 2
+
+MDAS
+#1 Create a Python program to perform arithmetic Operations  (Addition, Subtraction, Division, Multiplication, Modulus, Exponent) using two variable numbers for int, float and complex
+
+"""
+
+num1 = 10
+num2 = 5
+
+add = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+modu = num1 % num2
+expo = num1 ** num2
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Division:", div)
+print("Multiplication:", mul)
+print("Modulus:", modu)
+print("Exponent:", expo)
