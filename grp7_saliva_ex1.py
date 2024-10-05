14cbe11e5575c8a97b84944a3e7b334a4544039e
Ryan Saliva <144507791+yournote0814@users.noreply.github.com>
2024-10-05 15:45:01 +0800

    grp7_saliva_ex1.py

---
+++ grp7_saliva_ex1.py
@@ -0,0 +1,35 @@
+#My first Phyton Script
+
+#Bscs3f
+#Exercise 1
+
+print ("Hello, Im RYAN SALIVA BSCS 3F")
+
+age = 23
+name = "RYAN"
+gender = "Male"
+
+print("")
+
+print ("Name:",name)
+print ("Age:",age)
+print ("Gender:",gender)
+
+print("")
+kyla = "kyla"
+Joan = "Joan"
+Kenneth = "Kenneth"
+Sheramae = "Sheramae"
+
+print (kyla)
+print (Joan)
+print (Kenneth)
+print (Sheramae)
+
+
+ 
+print("")
+ 
+age,edad,num = 25 ,28, 23
+
+print(age,edad,num)
