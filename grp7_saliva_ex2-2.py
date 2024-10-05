00a656de70e89ed16473867966195b6fa2bb66bc
Ryan Saliva <144507791+yournote0814@users.noreply.github.com>
2024-10-05 16:11:24 +0800

    grp7_saliva_ex2-2.py

---
+++ grp7_saliva_ex2-2.py
@@ -0,0 +1,14 @@
+"""
+Name:
+BSCS 3F
+
+Exercise 2
+
+#2 Write a program that creates two variables, num1 and num2. Both num1 and num2 should be assigned the integer literal 25000000, one written with underscores and one without. Print num1 and num2 on two separate lines.
+
+"""
+num1 = 25000000
+num2 = 25_000_000
+
+print("Example 1 : ", num1)
+print("Example 2 :", num2)
